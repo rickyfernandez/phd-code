68ae0674245f75d837ff8889be32289f1997d50c
Ricardo Fernandez <rfernandez@astro.columbia.edu>
2014-08-18 09:14:29 -0500

    added exact riemann class

---
+++ PHD/riemann/exact.py
@@ -0,0 +1,13 @@
+from riemann_base import RiemannBase
+import numpy as np
+import riemann
+
+class Exact(RiemannBase):
+
+    def state(self, left_face, right_face, gamma):
+
+        num_faces = left_face.shape[1]
+        state_face = np.zeros((5,num_faces), dtype="float64")
+        riemann.exact(left_face, right_face, state_face, gamma, num_faces)
+
+        return state_face
