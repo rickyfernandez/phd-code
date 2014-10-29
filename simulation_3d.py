a60b0ee2021d9390561a823a0068d7afb3224804
Ricardo Fernandez <rfernandez@astro.columbia.edu>
2014-10-29 12:06:06 -0400

    added 3d simulation driver

--- simulation_3d.py
+++ simulation_3d.py
@@ -2,22 +2,22 @@ import PHD.riemann as riemann
 import PHD.boundary as boundary
 import PHD.simulation as simulation
 import PHD.reconstruction as reconstruction
-import PHD.test_problems.three_dim.toro_1_3d as toro_1_3d
+import PHD.test_problems.three_dim.toro_1 as toro_1
 
 # parameters and initial state of the simulation
-parameters, data, particles, particles_index = toro_1_3d.simulation()
+parameters, data, particles, particles_index = toro_1.simulation()
 
 # create boundary and riemann objects
 boundary_condition = boundary.Reflect3D(0.,1.,0.,.1,0.,.1)
-reconstruction = reconstruction.PiecewiseConstant(boundary_condition)
+reconstruction = reconstruction.PiecewiseConstant3D(boundary_condition)
 #reconstruction = reconstruction.PiecewiseLinear(boundary_condition)
-#riemann_solver = riemann.Exact(reconstruction)
-riemann_solver = riemann.Hllc3D(reconstruction)
-#riemann_solver = riemann.Hll(reconstruction)
+#riemann_solver = riemann.Exact3D(reconstruction)
+#riemann_solver = riemann.Hllc3D(reconstruction)
+riemann_solver = riemann.Hll3D(reconstruction)
 
 # setup the moving mesh simulation
-simulation = simulation.MovingMesh3D()
-#simulation = simulation.StaticMesh3D()
+#simulation = simulation.MovingMesh3D()
+simulation = simulation.StaticMesh3D()
 
 for key, value in parameters.iteritems():
     simulation.set_parameter(key, value)
