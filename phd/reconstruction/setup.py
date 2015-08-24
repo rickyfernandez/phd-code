43213a4022ba016bd281b6f0eb2ca6c10c74e154
Ricardo Fernandez <rafernandezjr@gmail.com>
2015-08-24 15:22:04 -0500

    moved unused files to attic folder

--- phd/reconstruction/setup.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(
-        cmdclass = {'build_ext': build_ext},
-        ext_modules = [Extension("reconstruction",["reconstruction.pyx"], include_dirs=[np.get_include()])])
--- phd/simulation/__init__.py
+++
@@ -1,4 +0,0 @@
-from static_mesh import StaticMesh
-from moving_mesh import MovingMesh
-from moving_mesh_3d import MovingMesh3D
-from static_mesh_3d import StaticMesh3D
