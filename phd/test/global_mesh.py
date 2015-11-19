ddd9fdf96aeb874441e7a1aece6b50d688e0426a
Ricardo Fernandez <rafernandezjr@gmail.com>
2015-11-19 16:25:15 -0600

    test works with changes to load balance

--- phd/test/global_mesh.py
+++ phd/test/global_mesh.py
@@ -7,27 +7,82 @@ from matplotlib.collections import LineCollection, PolyCollection, PatchCollecti
 from matplotlib.patches import Polygon
 from matplotlib.patches import Rectangle
 
+from utils.particle_tags import ParticleTAGS
 from domain.domain import DomainLimits
 from load_balance.load_balance import LoadBalance
-from particles.particle_array import ParticleArray
-from boundary.boundary import Boundary
-#from mesh.voronoi_mesh_2d import VoronoiMesh2D
+from containers.containers import ParticleContainer
+from boundary.boundary import MultiCoreBoundary
+from mesh.voronoi_mesh import VoronoiMesh2D
+
+def vor_plot(pc, mesh, rank):
+
+    # debugging plot --- turn to a routine later ---
+    l = []
+    ii = 0; jj = 0
+    for i in range(pc.get_number_of_particles()):
+
+        jj += mesh['number of neighbors'][i]*2
+
+        if pc['tag'][i] == ParticleTAGS.Real or pc['type'][i] == ParticleTAGS.Boundary or pc['type'][i] == ParticleTAGS.BoundarySecond:
+
+            verts_indices = np.unique(mesh['faces'][ii:jj])
+            verts = mesh['voronoi vertices'][verts_indices]
+
+            # coordinates of neighbors relative to particle p
+            xc = verts[:,0] - pc['position-x'][i]
+            yc = verts[:,1] - pc['position-y'][i]
+
+            # sort in counter clock wise order
+            sorted_vertices = np.argsort(np.angle(xc+1j*yc))
+            verts = verts[sorted_vertices]
+
+            l.append(Polygon(verts, True))
+
+        ii = jj
+
+    colors = []
+    for i in range(pc.get_number_of_particles()):
+        if pc['tag'][i] == ParticleTAGS.Real:
+            colors.append(0.25)
+        if pc['type'][i] == ParticleTAGS.Boundary:
+            colors.append(0.5)
+        if pc['type'][i] == ParticleTAGS.BoundarySecond:
+            colors.append(0.75)
+
+    fig, ax = plt.subplots()
+    p = PatchCollection(l, alpha=0.4)
+    p.set_array(np.array(colors))
+    p.set_clim([0.1, 1.0])
+
+    ax.set_xlim(-.1,1.1)
+    ax.set_ylim(-.1,1.1)
+    ax.add_collection(p)
+
+#    second = pc['type'] == 7
+#    print 'rank:', rank, np.sum(second)
+#    plt.scatter(pa['position-x'][second], pa['position-y'][second], color='lightsteelblue')
+
+    plt.savefig("plot_init_proc_%d.pdf" % rank, format='pdf')
+    plt.clf()
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+
 # generate random particles in a unit box for each process
 np.random.seed(rank)
 #my_num_particles = np.random.randint(64, 256)
-my_num_particles = np.random.randint(256, 512)
+my_num_particles = np.random.randint(512, 1024)
 my_particles = np.random.random(2*my_num_particles).reshape(2, my_num_particles).astype(np.float64)
 #my_particles = np.random.normal(0.5, 0.1, 2*my_num_particles).reshape(2, my_num_particles).astype(np.float64)
 
-pa = ParticleArray(my_num_particles)
+#pa = ParticleArray(my_num_particles)
+pa = ParticleContainer(my_num_particles)
 pa['position-x'][:] = my_particles[0,:]
 pa['position-y'][:] = my_particles[1,:]
 pa['process'][:] = rank
+pa['type'][:] = ParticleTAGS.Undefined
 
 #plot initial distribuition
 #plt.scatter(pc['position-x'], pc['position-y'])
@@ -42,30 +97,25 @@ pa['process'][:] = rank
 order = 21
 dom = DomainLimits(dim=2, xmin=0., xmax=1.)
 load_b = LoadBalance(pa, dom, comm=comm, order=order)
-global_tree = load_b.decomposition()
+load_b.decomposition()
+#global_tree = load_b.decomposition()
 
 #num_particles = pa.get_num_real_particles()
 
 #print 'rank: %d number of particles %d: number of leaves %d' % (rank, pc.num_real_particles, lb.global_work.size)
 
-#lb.create_ghost_particles()
-current_axis = plt.gca()
-# plot bounding leaf
-#for node in p:
-#    x = node[0]/2.0**order
-#    y = node[1]/2.0**order
-##    w = node[2]/2.0**order
-#    plt.plot(x, y, 'r*')
-##    current_axis.add_patch(Rectangle((x-.5*w, y-.5*w), w, w, color='orange', alpha=0.5))
-#
-# plot global tree
-current_axis = plt.gca()
-for node in global_tree.dump_data():
-    x = node[0]/2.0**order
-    y = node[1]/2.0**order
-    w = node[2]/2.0**order
-    current_axis.add_patch(Rectangle((x-.5*w, y-.5*w), w, w, fill=None))
-#
+
+
+
+
+
+
+
+
+
+
+
+
 ## create border particles
 #border = lb.create_ghost_particles(global_tree)
 #
@@ -78,20 +128,53 @@ for node in global_tree.dump_data():
 ##plt.ylim(-0.2,1.2)
 #plt.savefig("plot_proc_%d.png" % rank)
 #
+
+
 # plot particles
 #global_tree.create_boundary_particles(pa, rank, load_b.leaf_proc)
-bound = Boundary()
-bound.create_ghost_particles(pa, load_b.leaf_proc, global_tree, dom, comm)
-#print pa['tag']
-tag = pa['tag']
-real = tag == 0
-plt.scatter(pa['position-x'][real], pa['position-y'][real])
-plt.scatter(pa['position-x'][~real], pa['position-y'][~real], color="red")
-##plt.scatter(pc['position-x'][border], pc['position-y'][border], color="green")
-##plt.scatter(send_data['position-x'], send_data['position-y'], color="red")
-plt.xlim(-0.2,1.2)
-plt.ylim(-0.2,1.2)
-plt.savefig("plot_proc_%d.png" % rank)
+mesh = VoronoiMesh2D(pa)
+bound = MultiCoreBoundary()
+#bound.create_ghost_particles(pa, mesh, dom, load_b.leaf_proc, global_tree, comm)
+bound.create_ghost_particles(pa, mesh, dom, load_b, comm)
+mesh.tessellate()
+mesh.update_boundary_particles()
+mesh.update_second_boundary_particles()
+vor_plot(pa, mesh, rank)
+
+
+
+
+
+#tag = pa['tag']
+#real = tag == 0
+#plt.scatter(pa['position-x'][real], pa['position-y'][real], color='lightsteelblue')
+#
+## plot global tree
+#current_axis = plt.gca()
+#for node in global_tree.dump_data():
+#    x = node[0]/2.0**order
+#    y = node[1]/2.0**order
+#    w = node[2]/2.0**order
+#    current_axis.add_patch(Rectangle((x-.5*w, y-.5*w), w, w, fill=None))
+#
+#corner = pa['type'] == 10
+#plt.scatter(pa['position-x'][corner], pa['position-y'][corner], color="blue")
+#exterior = pa['type'] == 11
+#plt.scatter(pa['position-x'][exterior], pa['position-y'][exterior], color="red")
+#interior = pa['type'] == 12
+#plt.scatter(pa['position-x'][interior], pa['position-y'][interior], color="green")
+#
+#plt.xlim(-0.2,1.2)
+#plt.ylim(-0.2,1.2)
+#plt.savefig("plot_proc_%d.png" % rank)
+#plt.clf()
+
+
+
+
+
+
+
 #
 ## plot all the particles
 #sendbuf = np.array([my_num_particles], dtype=np.int32)
