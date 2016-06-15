a63214bd379203e522e5615a79e69cac8cc1608d
Ricardo Fernandez <rafernandezjr@gmail.com>
2016-06-14 22:38:05 -0500

    removed unused tags

--- phd/utils/particle_tags.py
+++ phd/utils/particle_tags.py
@@ -2,8 +2,6 @@
 class ParticleTAGS:
     Real = 0
     Ghost = 1
-    Exterior = 3
-    Interior = 4
-    ExportInterior = 5
-    OldGhost = 6
-    Undefined = 7
+    Exterior = 2
+    Interior = 3
+    Undefined = 4
