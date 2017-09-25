920b28d20e1c67c4ab2e0278e1d8c0409b56a293
Ricardo Fernandez <rafernandezjr@gmail.com>
2017-09-25 12:06:15 -0400

    more changes to integrate

--- phd/integrate/integrate.py
+++ phd/integrate/integrate.py
@@ -1,78 +1,158 @@
 import logging
 import numpy as np
 
+import phd
 from ..utils.tools import check_class
 
-from ..mesh.mesh import Mesh
-from ..boundary.boundary import Boundary
-from ..domain.domain import DomainLimits
-from ..riemann.riemann import RiemannBase
-from ..containers.containers import CarrayContainer
-from ..reconstruction.reconstruction import ReconstructionBase
-
 phdLogger = logging.getLogger('phd')
-
 callbacks = []
 
-class Eos(object):
-    pass
+class IntegrateBase(object):
+    def __init__(self, param_initial_time=0., param_final_time=1.0):
+        """Constructor for Integrate base class. Every integrate class has
+        to inherit this class.
+        """
+        self.param_final_time = param_final_time
+        self.param_initial_time = param_initial_time
 
-class NewIntegrateBase(object):
-    def __init__(self, time=0, final_time=1.0, iteration=0,
-            max_iteration=100000,**kwargs):
-        """Constructor for the Integrator"""
-        self.load_balance = None
-        self.particles = self.eos = None
-        self.reconstruction = self.riemann = None
-        self.mesh = self.domain = self.boundary = None
+        self.dt = 0.                    # time step
+        self.iteration = 0              # iteration number
+        self.time = param_initial_time  # current time
 
-        # simulation time
-        self.time = time
-        self.final_time = final_time
+        # required objects to be set
+        self.mesh = None
+        self.riemann = None
+        self.particles = None
+        self.reconstruction = None
+        self.domain_manager = None
 
-        # for paralel runs
-        self.is_parallel = False
-        self.loc_dt = np.zeros(1)
-        self.glb_dt = np.zeros(1)
+        # for communication dt across processors
+        self.loc_dt = np.zeros(1, dtype=np.float64)
+        self.glb_dt = np.zeros(1, dtype=np.float64)
 
-        # integration iteration number
+    def initialize(self):
+        """
+        Setup all connections for computation classes
+        """
+        msg = "IntegrateBase::initialize called!"
+        raise NotImplementedError(msg)
+
+    def compute_time_step(self):
+        '''
+        Compute time step for current state of the simulation.
+        Works in serial and parallel.
+        '''
+        msg = "IntegrateBase::compute_time_step called!"
+        raise NotImplementedError(msg)
+
+    def evolve_timestep(self):
+        """
+        Evolve the simulation for one time step
+        """
+        msg = "IntegrateBase::evolve_timestep called!"
+        raise NotImplementedError(msg)
+
+    def set_intial_time(self, initial_time):
+        '''Set current time'''
+        self.param_initial_time = initial_time
+        self.time = initial_time
+
+    def set_final_time(self, final_time):
+        '''Set current time'''
+        self.param_final_time = final_time
+
+    def set_iteration(self, iteration):
+        '''Set iteration count'''
         self.iteration = iteration
-        self.max_iteration = max_iteration
 
-    def initialize_and_link(self):
+    def set_dt(self, dt):
+        '''Set time step'''
+        self.dt = dt
+
+    @check_class(phd.DomainManager)
+    def set_domain_manager(self, domain_manager):
+        '''Set domain manager for communiating across processors'''
+        self.domain_manager = domain_manager
+
+    @check_class(phd.EquationStateBase)
+    def set_equation_state(self, equation_state):
+        '''Set equation of state for gas'''
+        self.equation_state = equation_state
+
+    @check_class(phd.CarrayContainer)
+    def set_particles(self, particles):
+        '''Set particles to simulate'''
+        self.particles = particles
+
+    @check_class(phd.Mesh)
+    def set_mesh(self, mesh):
+        '''Set spatial mesh'''
+        self.mesh = mesh
+
+    @check_class(phd.ReconstructionBase)
+    def set_reconstruction(self, reconstruction):
+        '''Set reconstruction method'''
+        self.reconstruction = reconstruction
+
+    @check_class(phd.RiemannBase)
+    def set_riemann(self, riemann):
+        '''Set riemann solver'''
+        self.riemann = riemann
+
+class StaticMesh(IntegrateBase):
+    '''
+    Static mesh integrator. Once the mesh is created in `begin_loop` method
+    the mesh will stay static throughout the simulation.
+    '''
+    def __init__(self, param_final_time=1.0):
+        """Constructor for the Integrator
         """
-        """
-        self._check_component()
+        super(StaticMesh, self).__init__(param_final_time)
 
-        # add load balance if in parallel 
-        self.domain_manager = DomainManager(self.is_paralell)
-        if self.is_parallel:
+    def initialize(self):
+        if not self.mesh or\
+                not self.riemann or\
+                not self.particles or\
+                not self.domain_manager or\
+                not self.reconstruction:
+            raise RuntimeError("Not all setters defined in StaticMesh")
 
-            # load balance
-            self.load_balance = LoadBalance()
-            self.load_balance.comm = self.comm
-            self.load_balance.domain = self.domain
-            self.load_balance._initialize()
+        # make sure proper dimension specified
+        dim = len(self.particles.named_groups["position"])
+        if dim != self.dim:
+            raise RuntimeError(
+                "Inconsistent dimension specified in particles %d and integrator %d)" %\
+                        (dim, self.dim))
 
-            # boundary
-            self.boundary.comm = self.comm
-            self.boundary.load_bal = self.load_balance
+        # initialize classes
+        self.domain_manager.initialize()
+        self.mesh.initialize()
 
-        # boundary class
-        self.boundary.domain = self.domain
-        self.boundary._initialize()
+        self.reconstruction.set_fields_for_reconstruction(self.particles)
+        self.reconstruction.initialize()
 
-        # mesh class
-        self.mesh.boundary = self.boundary
-        self.mesh._initialize()
+        self.riemann.set_feilds_for_flux(self.particles)
+        self.riemann.initialize()
 
-        # reconstruction class
-        self.reconstruction.mesh = self.mesh
-        self.reconstruction.particles = self.particles
-        self.reconstruction._initialize()
+    def begin_loop(self, simulation):
+        '''
+        Build initial mesh, the mesh is only built once.
+        '''
+        # ignored if in serial 
+        self.domain_manager.partition()
 
-        # riemann class
-        self.riemann.reconstruction = self.reconstruction
+        # build mesh with ghost particles and
+        # geometric quantities (volumes, faces, area, ...)
+        self.mesh.build_geometry(self.particles)
+
+        # relax mesh if needed TODO
+
+        # compute density, velocity, pressure, ...
+        self.equation_state.primitive_from_conserative(self.particles)
+
+        # assign velocities to mesh cells and faces 
+        self.mesh.assign_generator_velocities(self.particles)
+        self.mesh.assign_face_velocities()
 
     def compute_time_step(self):
         '''
@@ -80,110 +160,8 @@ class NewIntegrateBase(object):
         Works in serial and parallel.
         '''
         self.loc_dt[0] = self.riemann.compute_time_step()
-
-        # if in parallel find smallest dt across
-        # all meshes 
-        if self.is_parallel:
-            self.comm.Allreduce(sendbuf=local_dt,
-                    recvbuf=global_dt, op=MPI.MIN)
-            return self.glb_dt[0]
+        # if in parallel find smallest dt across TODO
         return self.loc_dt[0]
-
-    def finished(self):
-        '''
-        Check if the simulation has reached its final time or
-        reached max iteration number.
-        '''
-        return self.time >= self.final_time or\
-                self.iteration >= self.max_iteration
-
-    def evolve_timestep(self):
-        msg = "IntegrateBase::evolve_timestep called!"
-        raise NotImplementedError(msg)
-
-    def set_parallel(self, is_parallel):
-        '''Set parallel flag'''
-        self.is_parallel = is_parallel
-
-    def set_comm(self, comm):
-        '''Set mpi communicator'''
-        self.comm = comm
-
-    @check_class(Eos)
-    def set_eos(self, cl):
-        '''Set equation of state for gas'''
-        self.equation_state = cl
-
-    @check_class(CarrayContainer)
-    def set_particles(self, cl):
-        '''Set particles to simulate'''
-        self.particles = cl
-
-    @check_class(DomainLimits)
-    def set_domain(self, cl):
-        '''Set spatial domain exten'''
-        self.domain = cl
-
-    @check_class(Boundary)
-    def set_boundary(self, cl):
-        '''Set boundary generator'''
-        self.boundary = cl
-
-    @check_class(Mesh)
-    def set_mesh(self, cl):
-        '''Set spatial mesh'''
-        self.mesh = cl
-
-    @check_class(ReconstructionBase)
-    def set_reconstruction(self, cl):
-        '''Set reconstruction method'''
-        self.reconstruction = cl
-
-    @check_class(RiemannBase)
-    def set_riemann(self, cl):
-        '''Set riemann solver'''
-        self.riemann = cl
-
-class StaticMesh(NewIntegrateBase):
-    '''
-    Static mesh integrator. Once the mesh is created in `begin_loop` method
-    the mesh will stay static throughout the simulation.
-    '''
-    def __init__(self, time=0, final_time=1.0, iteration=0,
-            max_iteration=100000,**kwargs):
-
-        # call super
-        super(StaticMesh, self).__init__(time, final_time,
-                iteration, max_iteration,**kwargs)
-
-    def begin_loop(self, simulation):
-        '''
-        Build initial mesh, the mesh is only built once.
-        '''
-
-        # ignored if in serial 
-        self.domain_manager.parition()
-
-        # build mesh and geometric quantities
-        self.mesh.build_geometry(self.particles)
-
-        # relax mesh if needed
-        if self.mesh.relax_mesh and not simulation.restart:
-            for i in range(self.mesh.num_relax_iteration):
-
-                # lloyd relaxation
-                self.mesh.relax(self.particles)
-
-                # save data if requested
-                if simulation.save_mesh_relaxation:
-                    simulation.output_data()
-
-        # compute density, velocity, pressure, ...
-        self.equation_state.compute_primitive(self.particles)
-
-        # assign velocities to mesh cells and faces 
-        self.mesh.assign_mesh_generator_velocities(self.particles)
-        self.mesh.assign_face_velocities()
 
     def evolve_timestep(self):
         '''
@@ -192,56 +170,97 @@ class StaticMesh(NewIntegrateBase):
         phdLogger.info('Begining integration...')
 
         # solve the riemann problem at each face
-        self.reconstruction.compute_states(self.particles)
-        self.riemann.compute_fluxes(self.particles, self.reconstruction)
+        phdLogger.info('Moving Mesh Integrator - Solving riemann...')
+        self.reconstruction.compute_states(self.particles, self.mesh,
+                self.equation_state, self.riemann, self.dt)
+        phdLogger.success('Moving Mesh Integrator - Finished reconstruction')
+
+        phdLogger.info('Moving Mesh Integrator - Solving riemann...')
+        self.riemann.compute_fluxes(self.particles, self.mesh, self.reconstruction)
         self.mesh.update_from_fluxes(self.particles, self.riemann)
+        phdLogger.success('Moving Mesh Integrator - Finished riemann')
 
         # setup the mesh for the next setup 
-        self.eos.primitive_from_conserative(self.particles)
-        self.iteration_count += 1; current_time += dt
+        self.equation_state.primitive_from_conserative(self.particles)
+        self.iteration += 1; self.time += self.dt
 
         phdLogger.info('Finished integration')
 
     def after_loop(self):
         pass
 
-class MovingMesh(StaticMesh):
-    '''
-    Moving mesh integrator.
-    '''
-    def __init__(self, time=0, final_time=1.0, iteration=0,
-            max_iteration=100000,**kwargs):
+#class MovingMesh(StaticMesh):
+#    '''
+#    Moving mesh integrator.
+#    '''
+#    def __init__(self, param_time=0, param_final_time=1.0, param_iteration=0):
+#
+#        # call super
+#        self.param_time = param_time
+#        self.param_iteration = param_iteration
+#        self.param_final_time = param_final_time
+#
+#    def evolve_timestep(self):
+#        '''
+#        Solve the compressible gas equations
+#        '''
+#        phdLogger.info('Moving Mesh Integrator - Starting integration...')
+#
+#        # assign velocities to mesh cells and faces 
+#        self.mesh.assign_generator_velocities(self.particles)
+#        self.mesh.assign_face_velocities(self.particles)
+#
+#        # construct states at each face
+#        phdLogger.info('Moving Mesh Integrator - Computing reconstruction...')
+#        self.reconstruction.compute_states(self.particles, self.mesh,
+#                self.equation_state, self.riemann, dt)
+#        phdLogger.success('Moving Mesh Integrator - Finished reconstruction')
+#
+#        # solve riemann problem at each face and update variables
+#        phdLogger.info('Moving Mesh Integrator - Solving riemann...')
+#        self.riemann.compute_fluxes(self.particles, self.reconstruction)
+#        self.mesh.update_from_fluxes(self.particles, self.riemann, self.dt)
+#        phdLogger.success('Moving Mesh Integrator - Finished riemann')
+#
+#        # update mesh generator positions
+#        self.domain_manager.move_generators(self.particles)
+#        self.domain_manager.migrate_boundary_particles(self.particles)
+#
+#        # ignored if serial run
+#        if self.domain_manager.check_for_partion():
+#            phdLogger.info('Moving Mesh Integrator - Starting domain decomposition...')
+#            self.domain_manager.partion()
+#            phdLogger.success('Moving Mesh Integrator - Finished domain decomposition')
+#
+#        # setup the mesh for the next setup 
+#        phdLogger.info('Moving Mesh Integrator - Rebuilding mesh...')
+#        self.mesh.build_geometry(self.particles)
+#        phdLogger.success('Moving Mesh Integrator - Finished mesh')
+#
+#        self.equation_state.primitive_from_conserative(self.particles)
+#        self.equation_state.sound_speed(self.particles)
+#        self.iteration += 1; self.time += self.dt
+#
+#        phdLogger.success('Moving Mesh Integrator - Finished integration')
 
-        # call super
-        super(StaticMesh, self).__init__(time, final_time,
-                iteration, max_iteration,**kwargs)
 
-    def evolve_timestep(self):
-        '''
-        Solve the compressible gas equations
-        '''
-        phdLogger.info('Begining integration...')
-
-        # ignored if serial run
-        if self.domain_manager.check_for_partion():
-            self.domain_manager.partion()
-
-        # assign velocities to mesh cells and faces 
-        self.mesh.assign_mesh_generator_velocities(self.particles)
-        self.mesh.assign_face_velocities()
-
-        # solve the riemann problem at each face
-        self.reconstruction.compute_states(self.particles)
-        self.riemann.compute_fluxes(self.particles, self.reconstruction)
-        self.mesh.update_from_fluxes(self.particles, self.riemann)
-
-        # update mesh generator positions
-        self.mesh.move_generators(self.particles)
-        self.domain_manager.migrate_boundary_particles(self.particles)
-
-        # setup the mesh for the next setup 
-        self.mesh.build_geometry(self.particles)
-        self.eos.primitive_from_conserative(self.particles)
-
-        self.iteration_count += 1; current_time += dt
-        phdLogger.info('Finished integration')
+## --------------------- to add after serial working ---------------------------
+#class StaticMesh(IntegrateBase):
+#    def begin_loop(self, simulation):
+#        # relax mesh if needed
+#        if self.mesh.param_relax_mesh and not simulation.param_restart:
+#            for i in range(self.mesh.param_number_relaxations):
+#
+#                # lloyd relaxation
+#                self.mesh.relax(self.particles)
+#
+#                # output data if requested
+#                simulation.simulation_time(self)
+#    def compute_time_step(self):
+#
+#        # if in parallel find smallest dt across
+#        # all meshes 
+#        if phd._in_parallel:
+#            self.domain_manager.reduction(send=local_dt,
+#                    rec=global_dt, op='min')
+#            return self.glb_dt[0]
