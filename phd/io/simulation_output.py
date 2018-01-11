0dfb53675ff6ff60e9f1513997a673a00941cd1d
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-11 12:20:33 -0500

    added intitialize to outputs

--- phd/io/simulation_output.py
+++ phd/io/simulation_output.py
@@ -1,11 +1,14 @@
+import os
 
-class SimulationOutputerBase(object):
-    """Class that singals the simulation to write out
+from ..utils.logger import phdLogger
+
+class SimulationOutputterBase(object):
+    """Class that signals the simulation to write out
     data at current state of the simultion.
 
     This class is the api for all outputters. To inherit
-    you will have write your done, check_for_output,
-    done, and modify_timestep methods.
+    you will have write your own check_for_output and
+    modify_timestep methods.
 
     Attributes
     ----------
@@ -16,7 +19,10 @@ class SimulationOutputerBase(object):
     counter : int
         Output number in output order.
 
-    read_write : ReaderWriter 
+    output_directory : str
+        Directory to store all output of the simulation.
+
+    read_write : ReaderWriter
         Class that writes data in a given format.
 
     pad : int
@@ -27,19 +33,37 @@ class SimulationOutputerBase(object):
     def __init__(self, base_name, counter=0, pad=4):
         self.pad = pad
         self.counter = counter
-        self.read_write = None
-        self.base_name = base_name 
+        self.base_name = base_name
 
-    def initialize(self, simulation):
-        if not self.read_write:
-            raise RuntimeError("ERROR: Data writer not defined!")
+        # defined by setters
+        self.read_write = None
+        self.output_directory = None
+
+    def initialize(self):
+        """Initialize outputter, check for ReaderWriter and output directory."""
+        if not self.read_write or not self.output_directory:
+            raise RuntimeError("ERROR: Not all setters defined in %s!" %\
+                    self.__class__.__name__)
+
+        # directory to store data
+        self.data_directory = self.output_directory + "/" + self.base_name
+
+        if os.path.isdir(self.data_directory):
+            phdLogger.warning("Directory %s already exists, "
+                    "files maybe over written!" % self.data_directory)
+        else:
+            os.mkdir(self.data_directory)
+
+    def set_output_directory(self, output_directory):
+        """Set directory where to output files."""
+        self.output_directory = output_directory
 
     def set_writer(self, read_write):
         """Set class to write out data in a specific format.
 
         Parameters
         ----------
-        read_write : ReaderWriter 
+        read_write : ReaderWriter
             Class that writes data in a given format.
 
         """
@@ -59,46 +83,42 @@ class SimulationOutputerBase(object):
             True if time to output.
 
         """
-        msg = "SimulationOutputerBase::check_for_output called!"
+        msg = "SimulationOutputterBase::check_for_output called!"
         raise NotImplementedError(msg)
 
-    def done(self, simulation):
-        """Signal if outputter is done outputting.
+    def create_directory(self):
 
-        Parameters
-        ----------
-        simulation : Simulation
-           Class that marshalls the simulation of the fluid equations.
+        # new directory
+        output_directory = self.data_directory + "/" +\
+                self.base_name + str(self.counter).zfill(self.pad)
 
-        Returns
-        -------
-        bool
-            True if no more outputs to perform.
+        if os.path.isdir(output_directory):
+            phdLogger.warning("Directory %s already exists, "
+                    "files maybe over written!" % output_directory)
+        else:
+            os.mkdir(output_directory)
 
-        """
-        msg = "SimulationOutputerBase::done called!"
-        raise NotImplementedError(msg)
-
-    def output(self, output_directory, simulation):
+    def output(self, simulation):
         """Write out data to given directory.
 
         Parameters
         ----------
-        output_directory : str
-            Directory name to output data.
-
         simulation : Simulation
            Class that marshalls the simulation of the fluid equations.
 
         """
-        # skip if outputs are done
-        if self.done(simulation):
-            return
-
         if self.check_for_output(simulation):
+            self.create_directory()
+
+            output_directory = self.data_directory + "/" +\
+                self.base_name + str(self.counter).zfill(self.pad)
             file_name = self.base_name + str(self.counter).zfill(self.pad)
+
             self.read_write.write(
                     file_name, output_directory, simulation.integrator)
+
+            # create json file
+            self.write_parameters(file_name, self.output_directory, simulation)
             self.counter += 1
 
     def modify_timestep(self, simulation):
@@ -116,12 +136,20 @@ class SimulationOutputerBase(object):
             Modified dt if needed otherwise integrator dt.
 
         """
-        msg = "SimulationOutputerBase::modify_timestep called!"
+        msg = "SimulationOutputterBase::modify_timestep called!"
         raise NotImplementedError(msg)
 
-class IterationInterval(SimulationOutputerBase):
-    """Class that singals the simulation to write out
-    at given iteration intervals. 
+    def write_parameters(self, file_name, output_directory, simulation):
+        pass
+        # save parameter information
+        #param = save_parameters(simulation)
+        #with open(self.base_name + str(self.counter).zfill(self.pad), "w") as fp:
+        #    json.dump(param, fp, sort_keys=True, indent=4)
+
+
+class IterationInterval(SimulationOutputterBase):
+    """Class that signals the simulation to write out
+    at given iteration intervals.
 
     Attributes
     ----------
@@ -139,18 +167,14 @@ class IterationInterval(SimulationOutputerBase):
         Number of padding spaces for output numbering of
         data files.
 
-    read_write : ReaderWriter 
+    read_write : ReaderWriter
         Class that writes data in a given format.
 
     """
-    def __init__(self, iteration_interval, base_name="iteration_interval_",
+    def __init__(self, iteration_interval, base_name="iteration_interval",
                  counter=0, pad=4):
         super(IterationInterval, self).__init__(base_name, counter, pad)
         self.iteration_interval = iteration_interval
-
-    def done(self):
-        """Signal if outputter is done outputting."""
-        return False
 
     def check_for_output(self, simulation):
         """Return True to signal the simulation has reached
@@ -163,8 +187,9 @@ class IterationInterval(SimulationOutputerBase):
         # not modifying
         return simulation.integrator.dt
 
-class InitialOutput(SimulationOutputerBase):
-    """Class that singals the simulation to write out
+
+class InitialOutput(SimulationOutputterBase):
+    """Class that signals the simulation to write out
     initial data.
 
     Attributes
@@ -180,41 +205,27 @@ class InitialOutput(SimulationOutputerBase):
         Number of padding spaces for output numbering of
         data files.
 
-    read_write : ReaderWriter 
+    read_write : ReaderWriter
         Class that writes data in a given format.
-
-    simulation_started : bool
-        True if simaultion has entered compuation loop
 
     """
     def __init__(self, base_name="initial_output",
                  counter=0, pad=4):
         super(InitialOutput, self).__init__(base_name, counter, pad)
-        self.simulation_started = False
-
-    def done(self, simulation):
-        """Signal if outputter has already outputted initial data."""
-        if not self.simulation_started:
-            if simulation.state == SimulationTAGS.BEFORE_LOOP:
-                self.simulation_started = True
-            return False
-        return True
 
     def check_for_output(self, simulation):
         """Return True to signal first output before main loop."""
         return simulation.state == SimulationTAGS.BEFORE_LOOP
 
-    def output(self, output_directory, simulation):
+    def output(self, simulation):
         """Write out data to given directory."""
-        # skip if outputs are done
-        if self.done():
-            return
-
         if self.check_for_output(simulation):
             file_name = self.base_name
             self.read_write.write(
-                    file_name, output_directory, simulation.integrator)
-            self.counter += 1
+                    file_name, self.output_directory, simulation.integrator)
+
+            # create json file
+            self.write_parameters(file_name, self.output_directory, simulation)
 
     def modify_timestep(self, simulation):
         """Return consistent time step."""
@@ -222,8 +233,8 @@ class InitialOutput(SimulationOutputerBase):
         return simulation.integrator.dt
 
 
-class FinalOutput(SimulationOutputerBase):
-    """Class that singals the simulation to write out
+class FinalOutput(SimulationOutputterBase):
+    """Class that signals the simulation to write out
     final data.
 
     Attributes
@@ -239,37 +250,27 @@ class FinalOutput(SimulationOutputerBase):
         Number of padding spaces for output numbering of
         data files.
 
-    read_write : ReaderWriter 
+    read_write : ReaderWriter
         Class that writes data in a given format.
-
-    simulation_finished : bool
-        True if simaultion has exited compuation loop
 
     """
     def __init__(self, base_name="final_output",
                  counter=0, pad=4):
         super(InitialOutput, self).__init__(base_name, counter, pad)
-        self.simulation_finished = False
-
-    def done(self, simulation):
-        """Signal if outputter has already outputted final data."""
-        return False
 
     def check_for_output(self, simulation):
         """Return True to signal final output after main loop."""
         return simulation.state == SimulationTAGS.AFTER_LOOP
 
-    def output(self, output_directory, simulation):
+    def output(self, simulation):
         """Write out data to given directory."""
-        # skip if outputs are done
-        if self.done():
-            return
-
         if self.check_for_output(simulation):
             file_name = self.base_name
             self.read_write.write(
-                    file_name, output_directory, simulation.integrator)
-            self.counter += 1
+                    file_name, self.output_directory, simulation.integrator)
+
+            # create json file
+            self.write_parameters(file_name, self.output_directory, simulation)
 
     def modify_timestep(self, simulation):
         """Return consistent time step."""
@@ -277,8 +278,8 @@ class FinalOutput(SimulationOutputerBase):
         return simulation.integrator.dt
 
 
-class TimeInterval(SimulationOutputer):
-    """Class that singals the simulation to write out
+class TimeInterval(SimulationOutputterBase):
+    """Class that signals the simulation to write out
     final data.
 
     Attributes
@@ -297,7 +298,7 @@ class TimeInterval(SimulationOutputer):
     next_time_output : float
         Time of next output.
 
-    read_write : ReaderWriter 
+    read_write : ReaderWriter
         Class that writes data in a given format.
 
     time_interval : float
@@ -308,7 +309,7 @@ class TimeInterval(SimulationOutputer):
 
     """
     def __init__(self, time_interval, initial_time=0,
-                 base_name, counter, pad):
+                 base_name="time_interval", counter=0, pad=4):
         super(InitialOutput, self).__init__(base_name, counter, pad)
 
         self.time_interval = time_interval
@@ -319,83 +320,19 @@ class TimeInterval(SimulationOutputer):
         if not self.read_write:
             raise RuntimeError("ERROR: Data writer not defined!")
 
-    def done(self, simulation):
-        """Signal if outputter has already outputted time intervals."""
-        return False
-
-    def output(self, simulation):
+    def check_for_output(self, simulation):
         """Return True to signal the simulation has reached
         multiple of time_interval to ouput data.
         """
         integrator = simulation.integrator
-        if integrator.time >= self.time_last_output + self.time_interval:
-            self.time_last_output = integrator.time 
+        return (integrator.iteration % self.iteration_interval == 0)
+
+        integrator = simulation.integrator
+        if integrator.time >= self.next_time_output:
+            self.time_last_output = integrator.time
             self.next_time_output = self.time_last_output + self.time_interval
-            self.read_write.write()
+            return True
 
     def modify_timestep(self, simulation):
         """Return time step for next time interval output."""
         return self.next_time_output - simulation.integrator.time
-
-
-class SelectedTimes(SimulationOutputer):
-    """Class that singals the simulation to write out
-    data at selected times.
-
-    Attributes
-    ----------
-    base_name : str
-        Name of file to be outputted. Name will be post
-        fixed with output number.
-
-    counter : int
-        Output number in output order.
-
-    pad : int
-        Number of padding spaces for output numbering of
-        data files.
-
-    output_times : list
-        List of times to output at.
-
-    read_write : ReaderWriter 
-        Class that writes data in a given format.
-
-    times_not_done : np.array
-        Boolean array that signals which output times are left.
-
-    """
-    def __init__(self, output_times):
-
-        # TODO should remove output times instead of bool array
-        if type(output_times) is not list:
-            raise RuntimeError("ERROR: output_times needs to be type list")
-
-        self.output_times = np.asarray(output_times)
-        self.times_not_done = np.ones(
-                self.output_times.size,
-                dtype=bool)
-
-    def done(self, simulation):
-        """Signal if outputter has already outputted time intervals."""
-        return self.times_not_done.sum() == 0
-
-    def output(self, simulation):
-        """Return True to signal the simulation has reached
-        selected time to ouptput data.
-        """
-        integrator = simulation.integrator
-        if self.times_not_done.sum():
-            flag = integrator.time >= self.output_times[self.times_not_done]
-
-            self.times_not_done[flag] = False
-            self.read_write.write()
-
-    def modify_timestep(self, simulation):
-        """Return time step needed for next selected time."""
-        if not self.done():
-            integrator = simulation.integrator
-            dt = self.output_times[self.times_not_done] - integrator.time
-            return np.min(dt.min(), integrator.dt)
-        else:
-            return simulation.integrate.dt
