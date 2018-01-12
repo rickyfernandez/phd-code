10e11448c0ff9b11f93aa2574ed3442a02e9f92a
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-12 13:17:36 -0500

    redid test for reconstruction

--- phd/reconstruction/test/test_reconstruction.py
+++ phd/reconstruction/test/test_reconstruction.py
@@ -9,90 +9,53 @@ from phd.reconstruction.reconstruction import PieceWiseConstant
 
 from phd.riemann.riemann import RiemannBase
 
-class TestPieceWiseConstantSetup(unittest.TestCase):
+class TestPieceWiseConstant(unittest.TestCase):
     """Tests for the Reconstruction class."""
 
     def setUp(self):
 
         # setup particles and reconstruction class
-        self.particles = HydroParticleCreator(num=1, dim=2)
-        self.reconstruction = PieceWiseConstant()
+        self.particles = HydroParticleCreator(num=2, dim=2)
+        self.recon = PieceWiseConstant()
 
-    def test_set_fields_for_reconstruction_error(self):
+    def test_add_fields(self):
         particles = CarrayContainer(1, {"wrong_type": "int"})
-        particles.carray_named_groups["primitive"] = []
-        particles.carray_named_groups["primitive"].append("wrong_type")
 
+        # error for no primitive and velocity named group
         self.assertRaises(RuntimeError,
-                self.reconstruction.set_fields_for_reconstruction, particles)
-
-    def test_initialize_errors(self):
-        # fields for reconstruction not registered
-        self.assertRaises(RuntimeError, self.reconstruction.initialize)
-
-    def test_set_fields_for_reconstruction(self):
-
-       self.reconstruction.set_fields_for_reconstruction(self.particles)
-
-       # check for primitive fields, these are the fields that
-       # will be reconstructed
-       self.assertEqual(
-               self.reconstruction.reconstruct_field_groups["primitive"],
-               self.particles.carray_named_groups["primitive"])
-
-       # check for velocity fields
-       self.assertEqual(
-               self.reconstruction.reconstruct_field_groups["velocity"],
-               self.particles.carray_named_groups["velocity"])
-
-       # check if fields type are correct
-       for field in self.reconstruction.reconstruct_fields.keys():
-           self.assertEqual(
-                   self.reconstruction.reconstruct_fields[field],
-                   self.particles.carray_info[field])
+                self.recon.add_fields, particles)
 
     def test_initialize(self):
-       self.reconstruction.set_fields_for_reconstruction(self.particles)
+        # fields not registered
+        self.assertRaises(RuntimeError, self.recon.initialize)
 
-       # create left/right states containers
-       self.reconstruction.initialize()
+        self.recon.add_fields(self.particles)
+        self.recon.initialize()
 
-       # check if left states have correct fields
-       self.assertEqual(
-               self.reconstruction.left_states.carray_named_groups["primitive"],
-               self.particles.carray_named_groups["primitive"])
+        # check for left state primitive fields
+        self.assertItemsEqual(
+                self.recon.left_states.carray_named_groups["primitive"],
+                self.particles.carray_named_groups["primitive"])
+        self.assertItemsEqual(
+                self.recon.left_states.carray_named_groups["velocity"],
+                self.particles.carray_named_groups["velocity"])
 
-       # check if rigth states have correct fields
-       self.assertEqual(
-               self.reconstruction.right_states.carray_named_groups["primitive"],
-               self.particles.carray_named_groups["primitive"])
+        # check for right primitive fields
+        self.assertItemsEqual(
+                self.recon.right_states.carray_named_groups["primitive"],
+                self.particles.carray_named_groups["primitive"])
+        self.assertItemsEqual(
+                self.recon.right_states.carray_named_groups["velocity"],
+                self.particles.carray_named_groups["velocity"])
 
-       # check if left states have correct velocity fields
-       self.assertEqual(
-               self.reconstruction.left_states.carray_named_groups["velocity"],
-               self.particles.carray_named_groups["velocity"])
-
-       # check if rigth states have correct fields
-       self.assertEqual(
-               self.reconstruction.right_states.carray_named_groups["velocity"],
-               self.particles.carray_named_groups["velocity"])
-
-class TestPieceWiseConstantComputeStates(unittest.TestCase):
-    def setUp(self):
+    def test_compute_states(self):
 
         # create 2 particles with constant values
-        self.particles = HydroParticleCreator(num=2, dim=2)
         for field in self.particles.carray_named_groups["primitive"]:
             self.particles[field][:] = 1.0
 
-        # reconstruction class
-        self.reconstruction = PieceWiseConstant()
-        self.reconstruction.set_fields_for_reconstruction(
-                self.particles)
-        self.reconstruction.initialize()
-
-    def test_compute_states(self):
-        """Test reconstruction to left and right of face."""
+        self.recon.add_fields(self.particles)
+        self.recon.initialize()
 
         # domain manager
         domain_manager = DomainManager(0.2)
@@ -108,35 +71,16 @@ class TestPieceWiseConstantComputeStates(unittest.TestCase):
         mesh.faces["pair-j"][0] = 1  # right particle
 
         # construct left/right faces
-        self.reconstruction.compute_states(self.particles, mesh, False,
+        self.recon.compute_states(self.particles, mesh, False,
                 domain_manager, dt=1.0)
-        self.assertTrue(True)
 
         # left and right states should be the same
-        for field in self.reconstruction.left_states.properties.keys():
-            self.assertAlmostEqual(self.reconstruction.left_states[field][0],
+        for field in self.recon.left_states.carrays.keys():
+            self.assertAlmostEqual(self.recon.left_states[field][0],
                     self.particles[field][0])
-            self.assertAlmostEqual(self.reconstruction.right_states[field][0],
+
+            self.assertAlmostEqual(self.recon.right_states[field][0],
                     self.particles[field][1])
 
 if __name__ == "__main__":
     unittest.main()
-
-# patch rieman class
-#self.riemann_patch = patch('phd.riemann.riemann.RiemannBase', spec=True)
-#self.riemann = self.riemann_patch.start()
-
-
-#self.reconstruction = PieceWiseConstant()
-
-#def tearDown(self):
-#    self.riemann_patch.stop()
-#    self.domain_manager_patch.stop()
-# patch domain manager class
-#self.riemann_patch = patch('phd.riemann.riemann.RiemannBase', spec=RiemannBase)
-#self.riemann = self.riemann_patch.start()
-
-# patch equation of state
-#self.domain_manager_patch = patch('phd.domain.domain_manager.DomainManager',
-#        spec=DomainManager)
-#self.domain_manager = self.domain_manager_patch.start()
