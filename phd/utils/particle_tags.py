99093acde86dc81a462f11c2141ef11f25498166
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-14 11:55:38 -0600

    added simulation tags in particle_tags

--- phd/utils/particle_tags.py
+++ phd/utils/particle_tags.py
@@ -5,3 +5,10 @@ class ParticleTAGS:
     Exterior = 2
     Interior = 3
     Undefined = 4
+
+class SimulationTAGS:
+    PRE_EVOLVE = 0
+    BEFORE_LOOP = 1
+    MAIN_LOOP = 2
+    AFTER_LOOP = 3
+    POST_EVOLVE = 4
