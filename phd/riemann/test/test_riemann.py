35e36de46d5b42076d2a64381f2fe8054079b5b2
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-14 12:43:02 -0600

    properties -> carrays carray_info -> carray_dtypes

--- phd/riemann/test/test_riemann.py
+++ phd/riemann/test/test_riemann.py
@@ -39,7 +39,7 @@ class TestHLLSetup(unittest.TestCase):
        for field in self.riemann.flux_fields.keys():
            self.assertEqual(
                    self.riemann.flux_fields[field],
-                   self.particles.carray_info[field])
+                   self.particles.carrays_dtypes[field])
 
     def test_initialize(self):
        self.riemann.fields_to_add(self.particles)
@@ -108,7 +108,7 @@ class TestHLLFlux(unittest.TestCase):
         self.riemann.compute_fluxes(self.particles, self.mesh,
                 self.reconstruction, self.eos)
 
-        for field in self.riemann.fluxes.properties.keys():
+        for field in self.riemann.fluxes.carrays.keys():
             self.assertAlmostEqual(self.riemann.fluxes[field][0], ans[field])
 
         # reverse fluid flow
@@ -118,7 +118,7 @@ class TestHLLFlux(unittest.TestCase):
                 self.reconstruction, self.eos)
 
         # left and right states should be the same
-        for field in self.riemann.fluxes.properties.keys():
+        for field in self.riemann.fluxes.carrays.keys():
             self.assertAlmostEqual(self.riemann.fluxes[field][0], ans[field])
 
 if __name__ == "__main__":
