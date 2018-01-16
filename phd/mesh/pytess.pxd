003bcc085ad5da830c60190f09832f8065b6c8e7
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-16 16:56:56 -0600

    pytess functions back

--- phd/mesh/pytess.pxd
+++ phd/mesh/pytess.pxd
@@ -10,32 +10,30 @@ cdef extern from "tess.h":
     cdef cppclass Tess2d:
         Tess2d() except +
         void reset_tess()
-        #int build_initial_tess(double *x[3], double *radius_sq, int num_particles)
-        int build_initial_tess(double *x[3], double *radius_sq, int start_new_ghost, int stop_new_ghost)
-        int update_initial_tess(double *x[3], int begin_particles, int end_particles)
+        int build_initial_tess(double *x[3], double *radius, int start_ghost, int total_particles)
+        #int update_initial_tess(double *x[3], int begin_particles, int end_particles)
         int count_number_of_faces()
         int extract_geometry(double* x[3], double* dcenter_of_mass[3], double* volume,
                 double* face_area, double* face_com[3], double* face_n[3],
                 int* pair_i, int* pair_j, nn_vec &neighbors)
         int update_radius(double *x[3], double *radius, cpplist[FlagParticle] flagged_particles)
 
-#    cdef cppclass Tess3d:
-#        Tess3d() except +
-#        void reset_tess()
-#        int build_initial_tess(double *x[3], double *radius_sq, int num_particles, double huge)
-#        int build_initial_tess(double *x[3], double *radius_sq, int num_particles)
-#        int update_initial_tess(double *x[3], int up_num_particles)
-#        int count_number_of_faces()
-#        int extract_geometry(double* x[3], double* dcenter_of_mass[3], double* volume,
-#                double* face_area, double* face_com[3], double* face_n[3],
-#                int* pair_i, int* pair_j, nn_vec &neighbors)
+    cdef cppclass Tess3d:
+        Tess3d() except +
+        void reset_tess()
+        int build_initial_tess(double *x[3], double *radius, int start_ghost, int total_particles)
+        #int update_initial_tess(double *x[3], int begin_particles, int end_particles)
+        int count_number_of_faces()
+        int extract_geometry(double* x[3], double* dcenter_of_mass[3], double* volume,
+                double* face_area, double* face_com[3], double* face_n[3],
+                int* pair_i, int* pair_j, nn_vec &neighbors)
+        int update_radius(double *x[3], double *radius, cpplist[FlagParticle] flagged_particles)
 
 cdef class PyTess:
 
     cdef void reset_tess(self)
-    #cdef int build_initial_tess(self, double *x[3], double *radius_sq, int num_particles)
-    cdef int build_initial_tess(self, double *x[3], double *radius_sq, int start_new_ghost, int stop_new_ghost)
-    cdef int update_initial_tess(self, double *x[3], int begin_particles, int end_particles)
+    cdef int build_initial_tess(self, double *x[3], double *radius, int start_ghost, int total_particles)
+    #cdef int update_initial_tess(self, double *x[3], int begin_particles, int end_particles)
     cdef int count_number_of_faces(self)
     cdef int extract_geometry(self, double* x[3], double* dcenter_of_mass[3], double* volume,
                 double* face_area, double* face_com[3], double* face_n[3],
@@ -45,5 +43,5 @@ cdef class PyTess:
 cdef class PyTess2d(PyTess):
     cdef Tess2d *thisptr
 
-#cdef class PyTess3d(PyTess):
-#    cdef Tess3d *thisptr
+cdef class PyTess3d(PyTess):
+    cdef Tess3d *thisptr
--- phd/mesh/pytess.pyx
+++ phd/mesh/pytess.pyx
@@ -1,17 +1,16 @@
 
 cdef class PyTess:
     cdef void reset_tess(self):
-        raise NotImplementedError, 'PyTess::reset_tess'
+        raise NotImplementedError, "PyTess::reset_tess"
 
-    #cdef int build_initial_tess(self, double *x[3], double *radius_sq, int num_particles):
-    cdef int build_initial_tess(self, double *x[3], double *radius_sq, int start_new_ghost, int stop_new_ghost):
-        raise NotImplementedError, 'PyTess::build_initial_tess'
+    cdef int build_initial_tess(self, double *x[3], double *radius, int start_ghost, int total_particles):
+        raise NotImplementedError, "PyTess::build_initial_tess"
 
-    cdef int update_initial_tess(self, double *x[3], int begin_particles, int end_particles):
-        raise NotImplementedError, 'PyTess::update_initial_tess'
+    #cdef int update_initial_tess(self, double *x[3], int begin_particles, int end_particles):
+    #    raise NotImplementedError, "PyTess::update_initial_tess"
 
     cdef int count_number_of_faces(self):
-        raise NotImplementedError, 'PyTess::count_number_of_faces'
+        raise NotImplementedError, "PyTess::count_number_of_faces"
 
     cdef int extract_geometry(self, double* x[3], double* dcenter_of_mass[3], double* volume,
                 double* face_area, double* face_com[3], double* face_n[3],
@@ -20,6 +19,7 @@ cdef class PyTess:
 
     cdef int update_radius(self, double *x[3], double *radius, cpplist[FlagParticle] flagged_particles):
         pass
+
 
 cdef class PyTess2d(PyTess):
     def __cinit__(self):
@@ -31,13 +31,11 @@ cdef class PyTess2d(PyTess):
     cdef void reset_tess(self):
         self.thisptr.reset_tess()
 
-    #cdef int build_initial_tess(self, double *x[3], double *radius_sq, int num_particles):
-    cdef int build_initial_tess(self, double *x[3], double *radius_sq, int start_new_ghost, int stop_new_ghost):
-        #return self.thisptr.build_initial_tess(x, radius_sq, num_particles)
-        return self.thisptr.build_initial_tess(x, radius_sq, start_new_ghost, stop_new_ghost)
+    cdef int build_initial_tess(self, double *x[3], double *radius, int start_ghost, int total_particles):
+        return self.thisptr.build_initial_tess(x, radius, start_ghost, total_particles)
 
-    cdef int update_initial_tess(self, double *x[3], int begin_particles, int end_particles):
-        return self.thisptr.update_initial_tess(x, begin_particles, end_particles)
+    #cdef int update_initial_tess(self, double *x[3], int begin_particles, int end_particles):
+    #    return self.thisptr.update_initial_tess(x, begin_particles, end_particles)
 
     cdef int count_number_of_faces(self):
         return self.thisptr.count_number_of_faces()
@@ -52,28 +50,32 @@ cdef class PyTess2d(PyTess):
     cdef int update_radius(self, double *x[3], double *radius, cpplist[FlagParticle] flagged_particles):
         return self.thisptr.update_radius(x, radius, flagged_particles)
 
-#cdef class PyTess3d(PyTess):
-#    def __cinit__(self):
-#        self.thisptr = new Tess3d()
-#
-#    def __dealloc__(self):
-#        del self.thisptr
-#
-#    cdef void reset_tess(self):
-#        self.thisptr.reset_tess()
-#
-#    cdef int build_initial_tess(self, double *x[3], double *radius_sq, int num_particles, double huge):
-#        return self.thisptr.build_initial_tess(x, radius_sq, num_particles, huge)
-#
-#    cdef int update_initial_tess(self, double *x[3], int up_num_particles):
-#        return self.thisptr.update_initial_tess(x, up_num_particles)
-#
-#    cdef int count_number_of_faces(self):
-#        return self.thisptr.count_number_of_faces()
-#
-#    cdef int extract_geometry(self, double* x[3], double* dcenter_of_mass[3], double* volume,
-#                double* face_area, double* face_com[3], double* face_n[3],
-#                int* pair_i, int* pair_j, nn_vec &neighbors):
-#        return self.thisptr.extract_geometry(x, dcenter_of_mass, volume,
-#                face_area, face_com, face_n,
-#                pair_i, pair_j, neighbors)
+
+cdef class PyTess3d(PyTess):
+    def __cinit__(self):
+        self.thisptr = new Tess3d()
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    cdef void reset_tess(self):
+        self.thisptr.reset_tess()
+
+    cdef int build_initial_tess(self, double *x[3], double *radius, int start_ghost, int total_particles):
+        return self.thisptr.build_initial_tess(x, radius, start_ghost, total_particles)
+
+    #cdef int update_initial_tess(self, double *x[3], int up_num_particles):
+    #    return self.thisptr.update_initial_tess(x, up_num_particles)
+
+    cdef int count_number_of_faces(self):
+        return self.thisptr.count_number_of_faces()
+
+    cdef int extract_geometry(self, double* x[3], double* dcenter_of_mass[3], double* volume,
+                double* face_area, double* face_com[3], double* face_n[3],
+                int* pair_i, int* pair_j, nn_vec &neighbors):
+        return self.thisptr.extract_geometry(x, dcenter_of_mass, volume,
+                face_area, face_com, face_n,
+                pair_i, pair_j, neighbors)
+
+    cdef int update_radius(self, double *x[3], double *radius, cpplist[FlagParticle] flagged_particles):
+        return self.thisptr.update_radius(x, radius, flagged_particles)
