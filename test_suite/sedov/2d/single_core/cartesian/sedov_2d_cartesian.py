9c002864983c6fbfc4ff7ab8ad7f05e73bdd834b
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-18 19:39:29 -0600

    added sedov cartesian 2d example

--- test_suite/sedov/2d/single_core/cartesian/sedov_2d_cartesian.py
+++ test_suite/sedov/2d/single_core/cartesian/sedov_2d_cartesian.py
@@ -1,7 +1,7 @@
 import phd
 import numpy as np
 
-def create_particles(gamma):
+def create_particles(gamma=1.4):
 
     Lx = 1.    # domain size in x
     nx = 101   # particles per dim
@@ -10,47 +10,70 @@ def create_particles(gamma):
     dx = Lx/nx # spacing between particles
 
     # create particle container
-    pc = phd.HydroParticleCreator(n)
+    particles = phd.HydroParticleCreator(n)
     part = 0
     for i in range(nx):
         for j in range(nx):
-            pc['position-x'][part] = (i+0.5)*dx
-            pc['position-y'][part] = (j+0.5)*dx
-            pc['ids'][part] = part
+            particles["position-x"][part] = (i+0.5)*dx
+            particles["position-y"][part] = (j+0.5)*dx
+            particles["ids"][part] = part
             part += 1
 
     # set ambient values
-    pc['density'][:]  = 1.0               # density
-    pc['pressure'][:] = 1.0E-5*(gamma-1)  # total energy
+    particles["density"][:]  = 1.0               # density
+    particles["pressure"][:] = 1.0E-5*(gamma-1)  # total energy
 
     # put all enegery in center particle
     r = dx * .51
-    cells = ((pc['position-x']-.5)**2 + (pc['position-y']-.5)**2) <= r**2
-    pc['pressure'][cells] = 1.0/(dx*dx)*(gamma-1)
+    cells = ((particles["position-x"]-.5)**2 +\
+            (particles["position-y"]-.5)**2) <= r**2
+    particles["pressure"][cells] = 1.0/(dx*dx)*(gamma-1)
 
     # zero out velocities and set particle type
-    pc['velocity-x'][:] = 0.0
-    pc['velocity-y'][:] = 0.0
-    pc['tag'][:] = phd.ParticleTAGS.Real
-    pc['type'][:] = phd.ParticleTAGS.Undefined
+    particles["velocity-x"][:] = 0.0
+    particles["velocity-y"][:] = 0.0
+    particles["tag"][:] = phd.ParticleTAGS.Real
+    particles["type"][:] = phd.ParticleTAGS.Undefined
 
-    return pc
+    return particles
 
-# simulation driver
-sim = phd.Simulation(
-        cfl=0.5, tf=0.1, pfreq=1,
-        relax_num_iterations=0,
-        output_relax=False,
-        fname='sedov_2d_cartesian')
+# unit square domain
+minx = np.array([0., 0.])
+maxx = np.array([1., 1.])
+domain = phd.DomainLimits(minx, maxx)
 
-sim.add_particles(create_particles(1.4))                                  # create inital state of the simulation
-sim.add_domain(phd.DomainLimits(dim=2, xmin=0., xmax=1.))                 # spatial size of problem 
-sim.add_boundary(phd.Boundary(boundary_type=phd.BoundaryType.Reflective)) # reflective boundary condition
-sim.add_mesh(phd.Mesh())                                                  # tesselation algorithm
-sim.add_reconstruction(phd.PieceWiseLinear(limiter=1))                    # Linear reconstruction
-#sim.add_reconstruction(phd.PieceWiseConstant(limiter=1))                  # Linear reconstruction
-sim.add_riemann(phd.HLLC(gamma=1.4, boost=0))                             # riemann solver
-sim.add_integrator(phd.MovingMesh(regularize=1))                          # Integrator
+# computation related to boundaries
+domain_manager = phd.DomainManager(initial_radius=0.1,
+        search_radius_factor=1.25)
 
-# run the simulation
-sim.solve()
+# create voronoi mesh
+mesh = phd.Mesh(regularize=False)
+
+# computation
+integrator = phd.MovingMeshMUSCLHancock()
+integrator.set_mesh(mesh)
+integrator.set_domain_limits(domain)
+integrator.set_particles(create_particles())
+integrator.set_equation_state(phd.IdealGas())
+integrator.set_domain_manager(domain_manager)
+integrator.set_boundary_condition(phd.Reflective())
+integrator.set_reconstruction(phd.PieceWiseLinear(limiter=1))
+
+# add computation
+integrator.set_riemann(phd.HLLC(boost=True))
+
+# add finish criteria
+simulation_time_manager = phd.SimulationTimeManager()
+simulation_time_manager.add_finish(phd.Time(time_max=0.1))
+
+# output last step
+output = phd.FinalOutput()
+output.set_writer(phd.Hdf5())
+simulation_time_manager.add_output(output)
+
+# Create simulator
+simulation = phd.Simulation(simulation_name="sedov", colored_logs=True)
+simulation.set_integrator(integrator)
+simulation.set_simulation_time_manager(simulation_time_manager)
+simulation.initialize()
+simulation.solve()
