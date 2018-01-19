28a4f3dbbfd7823d8b7d0c4b5fda8b774de1d3e0
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-01-18 18:46:28 -0600

    added new sod version of sod example

--- test_suite/sod/2d/single_core/cartesian/sod_2d_cartesian.py
+++ test_suite/sod/2d/single_core/cartesian/sod_2d_cartesian.py
@@ -1,54 +1,77 @@
 import phd
 import numpy as np
 
-def create_particles(gamma):
+def create_particles(gamma=1.4):
 
     Lx = 1.    # domain size in x
-    nx = 100    # particles per dim
+    nx = 100   # particles per dim
     n = nx*nx  # number of points
 
     dx = Lx/nx # spacing between particles
 
     # create particle container
-    pc = phd.HydroParticleCreator(n)
+    particles = phd.HydroParticleCreator(n)
     part = 0
     for i in range(nx):
         for j in range(nx):
-            pc['position-x'][part] = (i+0.5)*dx
-            pc['position-y'][part] = (j+0.5)*dx
-            pc['ids'][part] = part
+            particles["position-x"][part] = (i+0.5)*dx
+            particles["position-y"][part] = (j+0.5)*dx
+            particles["ids"][part] = part
             part += 1
 
     # set ambient values
-    pc['density'][:]  = 1.0  # density
-    pc['pressure'][:] = 1.0  # total energy
+    particles["density"][:]  = 1.0  # density
+    particles["pressure"][:] = 1.0  # total energy
 
-    cells = pc['position-x'] > .5
-    pc['density'][cells] = 0.125
-    pc['pressure'][cells] = 0.1
+    cells = particles["position-x"] > .5
+    particles["density"][cells] = 0.125
+    particles["pressure"][cells] = 0.1
 
-    # zero out velocities and particle type
-    pc['velocity-x'][:] = 0.0
-    pc['velocity-y'][:] = 0.0
-    pc['tag'][:] = phd.ParticleTAGS.Real
-    pc['type'][:] = phd.ParticleTAGS.Undefined
+    # zero out velocities and set particle type
+    particles["velocity-x"][:] = 0.0
+    particles["velocity-y"][:] = 0.0
+    particles["tag"][:] = phd.ParticleTAGS.Real
+    particles["type"][:] = phd.ParticleTAGS.Undefined
 
-    return pc
+    return particles
 
-# simulation driver
-sim = phd.Simulation(
-        cfl=0.5, tf=0.15, pfreq=1,
-        relax_num_iterations=0,
-        output_relax=False,
-        fname='sod_2d_cartesian')
+# unit square domain
+minx = np.array([0., 0.])
+maxx = np.array([1., 1.])
+domain = phd.DomainLimits(minx, maxx)
 
-sim.add_particles(create_particles(1.4))                                  # create inital state of the simulation
-sim.add_domain(phd.DomainLimits(dim=2, xmin=0., xmax=1.))                 # spatial size of problem 
-sim.add_boundary(phd.Boundary(boundary_type=phd.BoundaryType.Reflective)) # reflective boundary condition
-sim.add_mesh(phd.Mesh())                                                  # tesselation algorithm
-sim.add_reconstruction(phd.PieceWiseLinear(limiter=0, boost=1))           # Linear reconstruction
-sim.add_riemann(phd.HLLC(gamma=1.4))                                      # riemann solver
-#sim.add_riemann(phd.Exact(gamma=1.4))                                      # riemann solver
-sim.add_integrator(phd.MovingMesh(regularize=1))                          # Integrator
+# computation related to boundaries
+domain_manager = phd.DomainManager(initial_radius=0.1,
+        search_radius_factor=1.25)
 
-sim.solve()
+# create voronoi mesh
+mesh = phd.Mesh(regularize=False)
+
+# computation
+integrator = phd.MovingMeshMUSCLHancock()
+integrator.set_mesh(mesh)
+integrator.set_domain_limits(domain)
+integrator.set_particles(create_particles())
+integrator.set_equation_state(phd.IdealGas())
+integrator.set_domain_manager(domain_manager)
+integrator.set_boundary_condition(phd.Reflective())
+integrator.set_reconstruction(phd.PieceWiseLinear())
+
+# add computation
+integrator.set_riemann(phd.HLLC(boost=True))
+
+# add finish criteria
+simulation_time_manager = phd.SimulationTimeManager()
+simulation_time_manager.add_finish(phd.Time(time_max=0.15))
+
+# output last step
+output = phd.FinalOutput()
+output.set_writer(phd.Hdf5())
+simulation_time_manager.add_output(output)
+
+# Create simulator
+simulation = phd.Simulation(simulation_name="sod", colored_logs=True)
+simulation.set_integrator(integrator)
+simulation.set_simulation_time_manager(simulation_time_manager)
+simulation.initialize()
+simulation.solve()
