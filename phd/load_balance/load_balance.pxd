261a28ed3c3be0f740334acbcdd4fcf7436dd61f
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-02-13 17:17:04 -0600

    removed DomainLimits from DomainManager

--- phd/load_balance/load_balance.pxd
+++ phd/load_balance/load_balance.pxd
@@ -1,7 +1,6 @@
 import numpy as np
 cimport numpy as np
 
-from ..domain.domain cimport DomainLimits
 from ..load_balance.tree cimport Tree, hilbert_type
 from ..utils.carray cimport LongArray, LongLongArray
 from ..containers.containers cimport CarrayContainer
@@ -9,30 +8,29 @@ from ..containers.containers cimport CarrayContainer
 
 cdef class LoadBalance:
 
-    cdef public DomainLimits domain
-    cdef public object comm
-    cdef public np.int32_t rank
-    cdef public np.int32_t size
-
     cdef public np.int32_t order
-
+    cdef public np.float64_t factor
     cdef public np.int32_t min_in_leaf
 
-    cdef public np.float64_t fac
-    cdef public np.ndarray corner
-    cdef public np.float64_t factor
-    cdef public np.float64_t box_length
+    cdef int dim
+    cdef np.float64_t fac
+    cdef double[2][3] bounds
 
-    cdef public export_ids
-    cdef public export_pid
+    cdef bint domain_info_added
 
-    cdef public Tree tree
-    cdef public LongArray leaf_pid
+    cdef double corner[3]
+    cdef np.float64_t box_length
+
+    cdef export_ids
+    cdef export_pid
+
+    cdef Tree tree
+    cdef LongArray leaf_pid
 
     cdef hilbert_type hilbert_func
 
-    cdef void _calculate_local_work(self, CarrayContainer pc, np.ndarray work)
-    cdef void _find_split_in_work(self, np.ndarray global_work)
-    cdef void _collect_particles_export(self, CarrayContainer pc, LongArray part_ids, LongArray part_pid,
+    cdef void calculate_local_work(self, CarrayContainer particles, np.ndarray work)
+    cdef void find_split_in_work(self, np.ndarray global_work)
+    cdef void collect_particles_export(self, CarrayContainer particles, LongArray part_ids, LongArray part_pid,
             LongArray leaf_pid, int my_pid)
-    cdef void _compute_hilbert_keys(self, CarrayContainer pc)
+    cdef void compute_hilbert_keys(self, CarrayContainer particles)
--- phd/load_balance/load_balance.pyx
+++ phd/load_balance/load_balance.pyx
@@ -1,10 +1,12 @@
+import phd
 import numpy as np
 from mpi4py import MPI
+
 from ..utils.particle_tags import ParticleTAGS
 from ..utils.exchange_particles import exchange_particles
 
 from .tree cimport Node
-from ..domain.domain cimport DomainLimits
+from ..domain.domain_manager cimport DomainManager
 from ..hilbert.hilbert cimport hilbert_key_2d, hilbert_key_3d
 
 
@@ -14,10 +16,6 @@ cdef class LoadBalance:
 
         Parameters
         ----------
-        domain : DomainLimits
-            Domain coordinate information.
-        comm : MPI.COMM_WORLD
-            MPI communicator.
         order : int
             The number of bits per dimension for constructing hilbert keys.
         """
@@ -25,47 +23,47 @@ cdef class LoadBalance:
         self.factor = factor
         self.min_in_leaf = min_in_leaf
 
+        self.domain_info_added = False
+
         self.leaf_pid = LongArray()
         self.export_ids = LongArray()
         self.export_pid = LongArray()
 
-    def _initialize(self):
-        """Constructor for load balance
+    def add_domain_info(self, DomainManager domain_manager):
+        cdef int i, k
 
-        Parameters
-        ----------
-        domain : DomainLimits
-            Domain coordinate information.
-        comm : MPI.COMM_WORLD
-            MPI communicator.
-        order : int
-            The number of bits per dimension for constructing hilbert keys.
+        for i in range(2):
+            for k in range(domain_manager.dim):
+                self.bounds[i][k] = domain_manager.bounds[i][k]
+
+        self.dim = domain_manager.dim
+        self.box_length = domain_manager.max_length
+        self.domain_info_added = True
+
+    def initialize(self):
+        """Constructor for load balance
         """
         cdef int i
-        cdef int dim = self.domain.dim
+        cdef np.ndarray corner = np.zeros(3)
 
-        self.rank = self.comm.Get_rank()
-        self.size = self.comm.Get_size()
+        if not self.domain_info_added:
+            raise RuntimeError("ERROR: Domain information not added")
+        self.fac = 2**self.order / self.box_length
 
-        fudge_factor = 1.001
-        self.box_length = self.domain.max_length#*fudge_factor
-        self.fac = (1 << self.order) / self.box_length
+        for i in range(self.dim):
+            corner[i] = self.bounds[0][i]
 
-        self.corner = np.zeros(3, dtype=np.float64)
-        for i in range(dim):
-            self.corner[i] = (self.domain.bounds[0][i] + self.domain.bounds[1][i])*0.5 - 0.5*self.box_length
-
-        if dim == 2:
+        if self.dim == 2:
             self.hilbert_func = hilbert_key_2d
-        elif dim == 3:
+        elif self.dim == 3:
             self.hilbert_func = hilbert_key_3d
         else:
             raise RuntimeError("Wrong dimension for tree")
 
-        self.tree = Tree(self.corner, self.box_length, dim,
+        self.tree = Tree(corner, self.box_length, self.dim,
                 self.factor, self.min_in_leaf, self.order)
 
-    def decomposition(self, CarrayContainer pc):
+    def decomposition(self, CarrayContainer particles):
         """Perform domain decomposition
         """
         cdef np.ndarray ind
@@ -76,26 +74,26 @@ cdef class LoadBalance:
         cdef int temp
 
         # remove current (if any) ghost particles
-        pc.remove_tagged_particles(ParticleTAGS.Ghost)
+        particles.remove_tagged_particles(ParticleTAGS.Ghost)
 
         # generate hilbert keys for real particles and create
         # global tree over all process
-        self._compute_hilbert_keys(pc)
-        self.tree.construct_global_tree(pc, self.comm)
+        self.compute_hilbert_keys(particles)
+        self.tree.construct_global_tree(particles, phd._comm)
 
         local_work  = np.zeros(self.tree.number_leaves, dtype=np.int32)
         global_work = np.zeros(self.tree.number_leaves, dtype=np.int32)
-        self._calculate_local_work(pc, local_work)
+        self.calculate_local_work(particles, local_work)
 
         # gather work across all processors
-        self.comm.Allreduce(sendbuf=local_work, recvbuf=global_work, op=MPI.SUM)
-        self._find_split_in_work(global_work)
+        phd._comm.Allreduce(sendbuf=local_work, recvbuf=global_work, op=MPI.SUM)
+        self.find_split_in_work(global_work)
 
         # collect particle for export
         self.export_ids.reset()
         self.export_pid.reset()
-        self._collect_particles_export(pc, self.export_ids, self.export_pid,
-                self.leaf_pid, self.rank)
+        self.collect_particles_export(particles, self.export_ids, self.export_pid,
+                self.leaf_pid, phd._rank)
 
         # arrange particles in process order
         export_ids_npy = self.export_ids.get_npy_array()
@@ -106,40 +104,40 @@ cdef class LoadBalance:
         export_pid_npy[:] = export_pid_npy[ind]
 
         # count number of particles to send to each process
-        recvbuf = np.zeros(self.size, dtype=np.int32)
+        recvbuf = np.zeros(phd._size, dtype=np.int32)
         sendbuf = np.bincount(export_pid_npy,
-                minlength=self.size).astype(np.int32)
+                minlength=phd._size).astype(np.int32)
 
         # how many particles are being sent from each process
-        self.comm.Alltoall(sendbuf=sendbuf, recvbuf=recvbuf)
+        phd._comm.Alltoall(sendbuf=sendbuf, recvbuf=recvbuf)
 
         # extract particles to send 
-        send_data = pc.get_sendbufs(export_ids_npy)
-        pc.remove_items(export_ids_npy)
-        temp = pc.get_carray_size()
+        send_data = particles.get_sendbufs(export_ids_npy)
+        particles.remove_items(export_ids_npy)
+        temp = particles.get_carray_size()
 
         # exchange load balance particles
-        pc.extend(np.sum(recvbuf))
-        exchange_particles(pc, send_data, sendbuf, recvbuf,
-                temp, self.comm)
+        particles.extend(np.sum(recvbuf))
+        exchange_particles(particles, send_data, sendbuf, recvbuf,
+                temp, phd._comm)
 
-        pc['process'][:] = self.rank
+        particles["process"][:] = phd._rank
 
-    cdef void _calculate_local_work(self, CarrayContainer pc, np.ndarray work):
+    cdef void calculate_local_work(self, CarrayContainer particles, np.ndarray work):
         """Calculate global work by calculating local work in each leaf. Then sum
         work across all process. Currently the work is just the the number of
         particles in each leaf.
         """
         cdef int i
         cdef Node* node
-        cdef LongLongArray keys = pc.get_carray("key")
+        cdef LongLongArray keys = particles.get_carray("key")
 
         # work is the number of local particles in leaf
-        for i in range(pc.get_carray_size()):
+        for i in range(particles.get_carray_size()):
             node = self.tree.find_leaf(keys.data[i])
             work[node.array_index] += 1
 
-    cdef void _find_split_in_work(self, np.ndarray global_work):
+    cdef void find_split_in_work(self, np.ndarray global_work):
         """Partition the global leaves amongst the process such that each process
         has roughly equal work load.
         """
@@ -149,10 +147,10 @@ cdef class LoadBalance:
         self.leaf_pid.resize(global_work.size)
 
         total_work = global_work.sum()
-        if total_work%self.size == 0:
-            part_per_proc = total_work/self.size
+        if total_work%phd._size == 0:
+            part_per_proc = total_work/phd._size
         else:
-            part_per_proc = total_work/self.size + 1
+            part_per_proc = total_work/phd._size + 1
 
         j = 1
         cum_sum = 0
@@ -162,7 +160,7 @@ cdef class LoadBalance:
                 j += 1
             self.leaf_pid.data[i] = j - 1
 
-    cdef void _collect_particles_export(self, CarrayContainer pc, LongArray part_ids,
+    cdef void collect_particles_export(self, CarrayContainer particles, LongArray part_ids,
         LongArray part_pid, LongArray leaf_pid, int my_pid):
         """
         Collect export particle indices and the process that it will be sent too.
@@ -183,9 +181,9 @@ cdef class LoadBalance:
         """
         cdef Node *node
         cdef int i, pid
-        cdef LongLongArray keys = pc.get_carray("key")
+        cdef LongLongArray keys = particles.get_carray("key")
 
-        for i in range(pc.get_carray_size()):
+        for i in range(particles.get_carray_size()):
 
             node = self.tree.find_leaf(keys.data[i])
             pid  = leaf_pid.data[node.array_index]
@@ -194,25 +192,25 @@ cdef class LoadBalance:
                 part_ids.append(i)
                 part_pid.append(pid)
 
-    cdef void _compute_hilbert_keys(self, CarrayContainer pc):
+    cdef void compute_hilbert_keys(self, CarrayContainer particles):
         """Compute hilbert key for each particle. The container is assumed to only have
         real particles.
 
         Parameters
         ----------
-        pc : CarrayContainer
+        particles : CarrayContainer
             Particle container holding all information of particles in the simulation.
         """
         cdef int i, j
         cdef Node* node
         cdef np.int32_t xh[3]
         cdef np.float64_t *x[3]
-        cdef LongLongArray keys = pc.get_carray("key")
+        cdef LongLongArray keys = particles.get_carray("key")
 
-        pc.pointer_groups(x, pc.carray_named_groups['position'])
+        particles.pointer_groups(x, particles.carray_named_groups["position"])
 
-        for i in range(pc.get_carray_size()):
+        for i in range(particles.get_carray_size()):
             for j in range(self.tree.dim):
-                xh[j] = <np.int32_t> ( (x[j][i] - self.corner[j])*self.fac )
+                xh[j] = <np.int32_t> ( (x[j][i] - self.tree.domain_corner[j])*self.fac )
 
             keys.data[i] = self.hilbert_func(xh[0], xh[1], xh[2], self.order)
