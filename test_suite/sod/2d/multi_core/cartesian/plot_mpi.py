acdd8033faaf1f32ade63abe3d3a62b12f42ae47
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-04-02 18:45:52 -0500

    plots in sod

---
+++ test_suite/sod/2d/multi_core/cartesian/plot_mpi.py
@@ -0,0 +1,63 @@
+import phd
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+
+# single-core solution
+file_name="sod_output/final_output/final_output0000/final_output0000.hdf5"
+reader = phd.Hdf5()
+sod = reader.read(file_name)
+
+# exact riemann solution
+f = h5py.File("riemann_sol.hdf5", "r")
+pos_ex = f["/x"][:]
+rho_ex = f["/density"][:]
+pre_ex = f["/pressure"][:]
+vel_ex = f["/velocity"][:]
+f.close()
+
+fig, axes = plt.subplots(2,2, figsize=(12,12))
+plt.suptitle("Sod Simulation")
+
+patch, colors = phd.vor_collection(sod, "density")
+sod.remove_tagged_particles(phd.ParticleTAGS.Ghost)
+
+p = PatchCollection(patch, edgecolor="black", linewidth=0.3, alpha=0.8)
+p.set_array(np.array(colors))
+p.set_clim([0, 1.0])
+ax = axes[0,0]
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_xlim(0,1)
+ax.set_ylim(0,1)
+ax.add_collection(p)
+
+ax = axes[0,1]
+ax.scatter(sod["position-x"], sod["density"], color="steelblue", label="simulation")
+ax.plot(pos_ex, rho_ex, "k", label="exact")
+ax.set_xlim(0,1.0)
+ax.set_ylim(0,1.1)
+ax.set_xlabel("X")
+ax.set_ylabel("Density")
+ax.legend()
+
+ax = axes[1,0]
+vel = np.sqrt(sod["velocity-x"]**2 + sod["velocity-y"]**2)
+ax.scatter(sod["position-x"], vel, color="steelblue")
+ax.plot(pos_ex, vel_ex, "k")
+ax.set_xlim(0,1)
+ax.set_ylim(-0.1,1.1)
+ax.set_xlabel("X")
+ax.set_ylabel("Velocity")
+
+ax = axes[1,1]
+ax.scatter(sod["position-x"], sod["pressure"], color="steelblue")
+ax.plot(pos_ex, pre_ex, "k")
+ax.set_xlim(0,1)
+ax.set_ylim(-0.1,1.1)
+ax.set_xlabel("X")
+ax.set_ylabel("Pressure")
+
+plt.savefig("sod_2d.png")
+plt.show()
--- test_suite/sod/2d/multi_core/cartesian/sod_2d_cartesian.py
+++ test_suite/sod/2d/multi_core/cartesian/sod_2d_cartesian.py
@@ -1,74 +1,41 @@
 import phd
 import numpy as np
 
-#to run:
-#$ mpirun -n 4 python sedov_2d_cartesian.py
+# to run:
+# $ mpirun -n 4 python sedov_2d_cartesian.py
+# for parallel or
+# $ python sedov_2d_cartesian.py
+# for single core
 
-if phd._rank == 0:
+def create_particles(dim=2, nx=100, Lx=1.0, diaphragm=0.5, gamma=1.4):
 
-    gamma = 1.4
-
-    Lx = 1.    # domain size in x
-    nx = 100   # particles per dim
+    dx = Lx/nx # spacing between particles
     n = nx*nx  # number of particles
 
-    dx = Lx/nx      # spacing between particles
-
     # create particle container
-    particles_root = phd.HydroParticleCreator(n)
+    particles = phd.HydroParticleCreator(n, dim=2)
     part = 0
+    np.random.seed(0)
     for i in range(nx):
         for j in range(nx):
-            particles_root["position-x"][part] = (i+0.5)*dx
-            particles_root["position-y"][part] = (j+0.5)*dx
-            particles_root["ids"][part] = part
+            particles_root["position-x"][part] = np.random.rand()
+            particles_root["position-y"][part] = np.random.rand()
+            particles["ids"][part] = part
             part += 1
 
     # set ambient values
-    particles_root["density"][:]  = 1.0  # density
-    particles_root["pressure"][:] = 1.0  # total energy
+    particles["density"][:]  = 1.0
+    particles["pressure"][:] = 1.0
+    particles["velocity-x"][:] = 0.0
+    particles["velocity-y"][:] = 0.0
 
-    cells = particles_root["position-x"] > .5
-    particles_root["density"][cells] = 0.125
-    particles_root["pressure"][cells] = 0.1
+    cells = particles["position-x"] > diaphragm
+    particles["density"][cells] = 0.125
+    particles["pressure"][cells] = 0.1
 
-    # how many particles to each process
-    nsect, extra = divmod(n, phd._size)
-    lengths = extra*[nsect+1] + (phd._size-extra)*[nsect]
-    send = np.array(lengths)
+    return particles
 
-    # how many particles 
-    disp = np.zeros(phd._size, dtype=np.int32)
-    for i in range(1, phd._size):
-        disp[i] = send[i-1] + disp[i-1]
-
-else:
-
-    lengths = disp = send = None
-    particles_root = {
-            "position-x": None,
-            "position-y": None,
-            "density": None,
-            "pressure": None,
-            "ids": None
-            }
-
-# tell each processor how many particles it will hold
-send = phd._comm.scatter(send, root=0)
-
-# allocate local particle container
-particles = phd.HydroParticleCreator(send)
-
-# import particles from root
-fields = ["position-x", "position-y", "density", "pressure", "ids"]
-for field in fields:
-    phd._comm.Scatterv([particles_root[field], (lengths, disp)], particles[field])
-
-particles["velocity-x"][:] = 0.0
-particles["velocity-y"][:] = 0.0
-#particles["process"][:] = phd._rank
-particles["tag"][:] = phd.ParticleTAGS.Real
-particles["type"][:] = phd.ParticleTAGS.Undefined
+particles = phd.distribute_initial_particles(create_particles, dim=2)
 
 # computation related to boundaries
 domain_manager = phd.DomainManager(
@@ -76,18 +43,22 @@ domain_manager = phd.DomainManager(
         initial_radius=0.1, search_radius_factor=2)
 
 # create voronoi mesh
-mesh = phd.Mesh(regularize=False, relax_iterations=0)
+mesh = phd.Mesh(regularize=False)
 
 # computation
 integrator = phd.MovingMeshMUSCLHancock()
 integrator.set_mesh(mesh)
+integrator.set_riemann(phd.HLLC())
 integrator.set_particles(particles)
-integrator.set_riemann(phd.HLLC(boost=True))
-integrator.set_equation_state(phd.IdealGas())
 integrator.set_domain_manager(domain_manager)
-integrator.set_load_balance(phd.LoadBalance())
 integrator.set_boundary_condition(phd.Reflective())
-integrator.set_reconstruction(phd.PieceWiseLinear(limiter=0))
+integrator.set_reconstruction(phd.PieceWiseLinear())
+integrator.set_equation_state(phd.IdealGas(gamma=1.4))
+
+sim_name = "sod"
+if phd._in_parallel:
+    integrator.set_load_balance(phd.LoadBalance())
+    sim_name = "mpi_sod"
 
 # add finish criteria
 simulation_time_manager = phd.SimulationTimeManager()
@@ -98,12 +69,8 @@ output = phd.FinalOutput()
 output.set_writer(phd.Hdf5())
 simulation_time_manager.add_output(output)
 
-output = phd.IterationInterval(iteration_interval=1)
-output.set_writer(phd.Hdf5())
-simulation_time_manager.add_output(output)
-
 # Create simulator
-simulation = phd.Simulation(simulation_name="sod", colored_logs=True)
+simulation = phd.Simulation(simulation_name=sim_name)
 simulation.set_integrator(integrator)
 simulation.set_simulation_time_manager(simulation_time_manager)
 simulation.initialize()
