78fe815904e57089d04d1e6ff8f099e2e58130d7
Ricardo Fernandez <rafernandezjr@gmail.com>
2018-04-09 18:00:47 -0500

    cleaned up rayleigh taylor example

--- test_suite/rayleigh_taylor/2d/single_core/rayleigh_2d_cartesian.py
+++ test_suite/rayleigh_taylor/2d/single_core/rayleigh_2d_cartesian.py
@@ -1,14 +1,11 @@
 import phd
 import numpy as np
 
-def create_particles(gamma=1.4):
+def create_particles(dim=2, nx=50, ny=150, gamma=1.4):
 
     Lx = 1.
-    nx = 64
-    dx = Lx/nx
-
     Ly = 3.
-    ny = 192
+    dx = Lx/nx
     dy = Ly/ny
 
     n = nx*ny
@@ -42,54 +39,53 @@ def create_particles(gamma=1.4):
 
             particles["position-x"][part] = x
             particles["position-y"][part] = y
-            particles["velocity-y"][part] = amp*np.cos(2.*np.pi*x/1.)*np.exp(-(y-center)**2/sigma)
+            particles["velocity-y"][part] = amp*np.cos(2.*np.pi*x/1.)*np.exp(-(y-center)**2/sigma**2)
             particles["ids"][part] = part
             part += 1
 
     # zero out velocities and set particle type
     particles["velocity-x"][:] = 0.0
-    particles["tag"][:] = phd.ParticleTAGS.Real
-    particles["type"][:] = phd.ParticleTAGS.Undefined
 
     return particles
 
+gamma = 1.4
+particles = create_particles(gamma=gamma)
 # computation related to boundaries
 domain_manager = phd.DomainManager(
-        xmin=[0., 0.], xmax=[1., 3.], initial_radius=0.1,
-        search_radius_factor=2)
+        xmin=[0., 0.], xmax=[1., 3.], initial_radius=0.1)
 
 # create voronoi mesh
-mesh = phd.Mesh(regularize=False, relax_iterations=0)
+mesh = phd.Mesh()
 
 # computation
 #integrator = phd.MovingMeshMUSCLHancock()
 integrator = phd.StaticMeshMUSCLHancock()
 integrator.set_mesh(mesh)
-integrator.set_riemann(phd.HLLC(boost=True))
+integrator.set_riemann(phd.HLLC())
 integrator.set_particles(create_particles())
-integrator.set_equation_state(phd.IdealGas())
 integrator.set_domain_manager(domain_manager)
 integrator.set_boundary_condition(phd.Reflective())
-integrator.set_reconstruction(phd.PieceWiseLinear(limiter=0))
+integrator.set_reconstruction(phd.PieceWiseLinear())
+integrator.set_equation_state(phd.IdealGas(gamma=gamma))
 
 # source term
 integrator.add_source_term(phd.ConstantGravity())
 
 # add finish criteria
 simulation_time_manager = phd.SimulationTimeManager()
-simulation_time_manager.add_finish(phd.Time(time_max=2.0))
+simulation_time_manager.add_finish(phd.Time(time_max=3.0))
 
 # output last step
 output = phd.FinalOutput()
 output.set_writer(phd.Hdf5())
 simulation_time_manager.add_output(output)
 
-output = phd.IterationInterval(iteration_interval=100)
+output = phd.TimeInterval(time_interval=0.1)
 output.set_writer(phd.Hdf5())
 simulation_time_manager.add_output(output)
 
 # Create simulator
-simulation = phd.Simulation(simulation_name="rt", colored_logs=True)
+simulation = phd.Simulation(simulation_name="rayleigh_static")
 simulation.set_integrator(integrator)
 simulation.set_simulation_time_manager(simulation_time_manager)
 simulation.initialize()
---
+++ test_suite/rayleigh_taylor/2d/single_core/rayleigh_density_panel.py
@@ -0,0 +1,57 @@
+import phd
+import h5py
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+
+mpl.rcParams["mathtext.fontset"] = "stix"
+mpl.rcParams["font.family"] = "STIXGeneral"
+mpl.rcParams["xtick.labelsize"] = 16
+mpl.rcParams["ytick.labelsize"] = 16
+
+# single-core solution
+static_names=[
+        ["rayleigh_static_output/time_interval/time_interval0004/time_interval0004.hdf5",
+         "rayleigh_static_output/time_interval/time_interval0009/time_interval0009.hdf5",
+         "rayleigh_static_output/time_interval/time_interval0014/time_interval0014.hdf5",
+         "rayleigh_static_output/time_interval/time_interval0019/time_interval0019.hdf5"],
+        ["rayleigh_moving_output/time_interval/time_interval0004/time_interval0004.hdf5",
+         "rayleigh_moving_output/time_interval/time_interval0009/time_interval0009.hdf5",
+         "rayleigh_moving_output/time_interval/time_interval0014/time_interval0014.hdf5",
+         "rayleigh_moving_output/time_interval/time_interval0019/time_interval0019.hdf5"]]
+
+fig, axes = plt.subplots(2,4, figsize=(8,12), sharey=True, sharex=True)
+plt.subplots_adjust(wspace=0.1, hspace=0.04, top=0.98, bottom=0.02, left=0.02, right=0.98)
+
+reader = phd.Hdf5()
+for i in range(4): # over columns
+    for j in range(2): # over plots
+
+        file_name = static_names[j][i]
+        particles = reader.read(file_name)
+        time = h5py.File(file_name, "r").attrs["time"]
+
+        ax = axes[j,i]
+        patch, colors = phd.vor_collection(particles, "density")
+        particles.remove_tagged_particles(phd.ParticleTAGS.Ghost)
+
+        print particles["density"].min(), particles["density"].max()
+
+        p = PatchCollection(patch, cmap="jet", edgecolor="none")
+        p.set_array(np.array(colors))
+        p.set_clim([0.9, 2.1])
+        ax.add_collection(p)
+        ax.set_xlim(0,1)
+        ax.set_ylim(0,3)
+
+        ax.text(0.07, 2.80, r"$t=%0.2f$" % time, fontsize=14,
+                bbox=dict(boxstyle="round", facecolor="white"))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect("auto")
+
+plt.savefig("rayleigh_compare.eps")
+plt.show()
